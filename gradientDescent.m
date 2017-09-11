d045a1322821f4a70524e6bbb741d0432891730b
udasahai <udasahai@Udayans-MacBook-Pro.local>
2017-09-12 02:34:49 +0530

    All mandatory part done.

--- gradientDescent.m
+++ gradientDescent.m
@@ -7,6 +7,7 @@ function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
 m = length(y); % number of training examples
 J_history = zeros(num_iters, 1);
 
+
 for iter = 1:num_iters
 
     % ====================== YOUR CODE HERE ======================
@@ -16,6 +17,18 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
+
+
+    tempVector = ( X' * ((X*theta) - y)); 
+    tempVector = sum(tempVector,2);
+
+    % printf ("%f %f\n", tempVector(1,1), tempVector(2,1));
+
+    constant = alpha/m; 
+    tempVector = tempVector*constant; 
+    theta = theta - tempVector;  
+
+    % printf ("%f %f\n", theta(1,1), theta(2,1));
 
 
 
